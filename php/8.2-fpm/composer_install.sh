7deee9ff0b55c330645746a5d2c2063849f51e11
Jalen Davenport <jalen@jalendport.com>
2024-07-11 16:20:58 -0400

    Update the entrypoint and cmd for php images

---
+++ php/8.2-fpm/composer_install.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+set -e
+
+echo "Checking for composer.json and the absence of composer.lock or vendor..."
+if [ -f "composer.json" ] && ( [ ! -f "composer.lock" ] || [ ! -d "vendor" ] ); then
+	echo "Running composer install..."
+	composer \
+		--no-ansi \
+		--no-dev \
+		--no-interaction \
+		--no-progress \
+		--no-scripts \
+		--optimize-autoloader \
+		install
+else
+	echo "No composer installation needed."
+fi
