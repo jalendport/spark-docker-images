86b35068bbedf913d07e8598da0deb0da21ff2dc
Jalen Davenport <jalen@jalendport.com>
2025-09-02 21:53:41 -0400

    Add PHP 8.4; add image libraries to PHP images

---
+++ php/8.4-cli/composer_install.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+set -e
+
+echo "Checking for composer.json and the absence of composer.lock or vendor..."
+if [ -f "composer.json" ] && ( [ ! -f "composer.lock" ] || [ ! -d "vendor" ] ); then
+	echo "Running composer install..."
+	composer \
+		--no-ansi \
+		--no-interaction \
+		--no-progress \
+		--no-scripts \
+		--optimize-autoloader \
+		install
+else
+	echo "No composer installation needed."
+fi
+
+exec "$@"
